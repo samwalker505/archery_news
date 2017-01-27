1f8bbc66ae1f6a03a5bad32db661c7c433409676
samwalker <samwalker505@gmail.com>
2017-01-28 01:28:57 +0800

    changed to use while to fetch webpage

--- cron_jobs/archery_org_hk_spider.py
+++ cron_jobs/archery_org_hk_spider.py
@@ -14,12 +14,13 @@ class ArcheryOrgHkSpiderHandler(BaseHandler):
 
     @Output.json(response_type='array')
     def get(self, *args, **kwargs):
+        pageNum = 0
+        url_format = 'https://www.archery.org.hk/frontpage?page={}'
+        url = url_format.format(pageNum)
         results = []
-        def handle_result(rpc):
-            result = rpc.get_result()
-            # self.response.write(result.content)
-            logging.info('Handling RPC in callback: result {}'.format(result))
-            page = etree.HTML(result.content)
+        while url:
+            get_result = urlfetch.fetch(url)
+            page = etree.HTML(get_result.content)
 
             for row in page.xpath('//div[contains(@class, "views-row")]'):
                 title = row.xpath('descendant::h2/a/text()')[0]
@@ -42,29 +43,18 @@ class ArcheryOrgHkSpiderHandler(BaseHandler):
                 }
                 results.append(result)
 
+            if page.xpath('//li[contains(@class, "pager-last")]'):
+                pageNum += 1
+                url = url_format.format(pageNum)
+            else:
+                url = None
 
-        urls = ['https://www.archery.org.hk',
-                'https://www.archery.org.hk/frontpage?page=1',
-                'https://www.archery.org.hk/frontpage?page=2',
-                'https://www.archery.org.hk/frontpage?page=3',
-                ]
-        import functools
-        rpcs = []
-        for url in urls:
-            rpc = urlfetch.create_rpc()
-            rpc.callback = functools.partial(handle_result, rpc)
-            urlfetch.make_fetch_call(rpc, url)
-            rpcs.append(rpc)
-
-        for rpc in rpcs:
-            rpc.wait()
-        logging.debug('done waiting')
+        logging.debug('done fetching')
         result_ids = ['{}:{}'.format(r['url_hash'], r['content_hash']) for r in results]
 
         keys = [ndb.Key(ArcheryOrgHkPost, str(rid)) for rid in result_ids]
         created_posts = ndb.get_multi(keys)
         to_create = [r for r, p in zip(results, created_posts) if not p]
-        logging.debug('to_create: {}'.format(to_create))
         posts = ArcheryOrgHkPost.create_from_spider_batch(to_create)
 
         return {'results': [post.to_dict_for_owner() for post in posts]}
